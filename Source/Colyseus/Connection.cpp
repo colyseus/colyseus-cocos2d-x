93aac45b85deed437d4ad7a5ae6daa9cc248bc86
Endel Dreyer <endel.dreyer@gmail.com>
2019-03-07 23:32:15 -0300

    remove std::cout debugs

--- Source/Colyseus/Connection.cpp
+++ Source/Colyseus/Connection.cpp
@@ -20,10 +20,6 @@ Connection::~Connection()
 
 void Connection::open()
 {
-    std::cout << std::endl;
-    std::cout << "Connection:open() => " << endpoint << std::endl;
-    std::cout << std::endl;
-    
     _ws = new WebSocket();
     if(!_ws->init(*this, endpoint))
     {
